f036397efb2cdd0ad9e0ee2ee96269551f4ee813
AndrewDunneBP <106807488+AndrewDunneBP@users.noreply.github.com>
2022-12-09 16:06:30 +0000

    Original Scripts

--- yatzy-andrew-gavin-typescript/src/Yatzy.ts
+++ yatzy-andrew-gavin-typescript/src/Yatzy.ts
@@ -1,49 +1,69 @@
 export default class Yatzy {
   private dice: number[];
 
-  constructor(d1: number, d2: number, d3: number, d4: number, d5: number) {
-    this.dice = [d1, d2, d3, d4, d5];
+  constructor(d1: number, d2: number, d3: number, d4: number, _5: number) {
+    this.dice = [];
+    this.dice[0] = d1;
+    this.dice[1] = d2;
+    this.dice[2] = d3;
+    this.dice[3] = d4;
+    this.dice[4] = _5;
   }
 
-  chance(): number {
-    return this.dice.reduce((accumulator, current) => accumulator + current, 0)
+  static chance(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    var total = 0;
+    total += d1;
+    total += d2;
+    total += d3;
+    total += d4;
+    total += d5;
+    return total;
   }
 
-  yatzy(): number {
-    return this.dice.every(e => e === this.dice[0]) ? 50 : 0;
+  static yatzy(...args: number[]): number {
+    var counts = [0, 0, 0, 0, 0, 0, 0, 0];
+    for (var i = 0; i != args.length; ++i) {
+      var die = args[i];
+      counts[die - 1]++;
+    }
+    for (i = 0; i != 6; i++) if (counts[i] == 5) return 50;
+    return 0;
   }
 
-  ones(): number {
-    return this.sameNumber(1);
+  static ones(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    var sum = 0;
+    if (d1 == 1) sum++;
+    if (d2 == 1) sum++;
+    if (d3 == 1) sum++;
+    if (d4 == 1) sum++;
+    if (d5 == 1) sum++;
+
+    return sum;
   }
 
-  twos(): number {
-    return this.sameNumber(2);
+  static twos(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    var sum = 0;
+    if (d1 == 2) sum += 2;
+    if (d2 == 2) sum += 2;
+    if (d3 == 2) sum += 2;
+    if (d4 == 2) sum += 2;
+    if (d5 == 2) sum += 2;
+    return sum;
   }
 
-  threes(): number {
-    return this.sameNumber(3);
+  static threes(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    var s;
+    s = 0;
+    if (d1 == 3) s += 3;
+    if (d2 == 3) s += 3;
+    if (d3 == 3) s += 3;
+    if (d4 == 3) s += 3;
+    if (d5 == 3) s += 3;
+    return s;
   }
 
-  fours(): number {
-    return this.sameNumber(4);
-  }
-
-  fives(): number {
-    return this.sameNumber(5);
-  }
- 
-  sixes(): number {
-    return this.sameNumber(6);
-  }
-
-  sameNumber(num: number): number {
-    return this.dice.filter(d => d === num).length * num;
-  }
-
-  one_pair(): number {
-    /* Working on this refactor
-    var counts = [0, 0, 0, 0, 0, 0];
+  static score_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     counts[d1 - 1]++;
     counts[d2 - 1]++;
     counts[d3 - 1]++;
@@ -51,7 +71,6 @@ export default class Yatzy {
     counts[d5 - 1]++;
     var at;
     for (at = 0; at != 6; at++) if (counts[6 - at - 1] >= 2) return (6 - at) * 2;
-    */
     return 0;
   }
 
@@ -73,18 +92,6 @@ export default class Yatzy {
     else return 0;
   }
 
-  static three_of_a_kind(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var t;
-    t = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    t[d1 - 1]++;
-    t[d2 - 1]++;
-    t[d3 - 1]++;
-    t[d4 - 1]++;
-    t[d5 - 1]++;
-    for (let i = 0; i < 6; i++) if (t[i] >= 3) return (i + 1) * 3;
-    return 0;
-  }
-
   static four_of_a_kind(_1: number, _2: number, d3: number, d4: number, d5: number): number {
     var tallies;
     tallies = [0, 0, 0, 0, 0, 0, 0, 0];
@@ -94,6 +101,18 @@ export default class Yatzy {
     tallies[d4 - 1]++;
     tallies[d5 - 1]++;
     for (let i = 0; i < 6; i++) if (tallies[i] >= 4) return (i + 1) * 4;
+    return 0;
+  }
+
+  static three_of_a_kind(d1: number, d2: number, d3: number, d4: number, d5: number): number {
+    var t;
+    t = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    t[d1 - 1]++;
+    t[d2 - 1]++;
+    t[d3 - 1]++;
+    t[d4 - 1]++;
+    t[d5 - 1]++;
+    for (let i = 0; i < 6; i++) if (t[i] >= 3) return (i + 1) * 3;
     return 0;
   }
 
@@ -150,5 +169,29 @@ export default class Yatzy {
 
     if (_2 && _3) return _2_at * 2 + _3_at * 3;
     else return 0;
+  }
+
+  fours(): number {
+    var sum;
+    sum = 0;
+    for (let at = 0; at != 5; at++) {
+      if (this.dice[at] == 4) {
+        sum += 4;
+      }
+    }
+    return sum;
+  }
+
+  fives(): number {
+    let s = 0;
+    var i;
+    for (i = 0; i < this.dice.length; i++) if (this.dice[i] == 5) s = s + 5;
+    return s;
+  }
+
+  sixes(): number {
+    let sum = 0;
+    for (var at = 0; at < this.dice.length; at++) if (this.dice[at] == 6) sum = sum + 6;
+    return sum;
   }
 }
